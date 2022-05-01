20027ef294fde4368e5e30fdef40524ecd1feb25
MuriloM <muriloommaia@gmail.com>
2022-05-01 19:39:07 -0300

    feat: reinstall eslint

--- kimchechallenge/.eslintrc.js
+++ kimchechallenge/.eslintrc.js
@@ -1,27 +1,30 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
-    ],
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-        "react/react-in-jsx-scope": "off"
-      }
+    parser: 'babel-eslint',
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'jsx-filename-extension': 'off',
+    'import/prefer-default-export': 'off',
+    'jsx-quotes': ['error', 'prefer-single']
+  },
 };
