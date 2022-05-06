3bdf05ea2c3191b453e9c04a9c0dd532fbb09a74
MuriloM <muriloommaia@gmail.com>
2022-05-06 11:57:38 -0300

    feat: implement Box group

--- kimchechallenge/src/components/Organisms/ResultField/BoxGroup.js
+++ kimchechallenge/src/components/Organisms/ResultField/BoxGroup.js
@@ -1,17 +1,26 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import PropTypes from 'prop-types';
 import React from 'react';
+import { GridComponent } from '../../Containers/Grid';
+import UniqueBox from './UniqueBox';
 
-export default function BoxGroup({ items }) {
+export default function BoxGroup({ items, name }) {
   return (
     <div>
-      UniqueBox
-      {items}
+      <p>
+        {name}
+      </p>
+      <GridComponent cols={3}>
+        {items.map((country) => (
+          <UniqueBox key={country.name} country={country} />
+        ))}
+      </GridComponent>
     </div>
   );
 }
 
 BoxGroup.propTypes = {
   // eslint-disable-next-line react/forbid-prop-types
-  items: PropTypes.arrayOf(PropTypes.any).isRequired,
+  items: PropTypes.any.isRequired,
+  name: PropTypes.string.isRequired,
 };
