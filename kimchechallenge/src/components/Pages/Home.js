66118d8e5ecfe0c41187002e5df5a7592c5e38c7
MuriloM <muriloommaia@gmail.com>
2022-05-06 00:22:11 -0300

    fix: change path in Home

--- kimchechallenge/src/components/Pages/Home.js
+++ kimchechallenge/src/components/Pages/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { ContainerResult } from '../Containers/Container.Result';
 import Filters from '../Organisms/Filters';
 import Header from '../Organisms/Header';
-import SearchResult from '../Organisms/SearchResult';
+import SearchResult from '../Organisms/ResultField/SearchResult';
 
 export default function Home() {
   return (
