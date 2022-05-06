d12b9fe2ffec15c4b7c9668a099264645db931b3
MuriloM <muriloommaia@gmail.com>
2022-05-05 23:59:16 -0300

    feat: create container result

---
+++ kimchechallenge/src/components/Containers/Container.Result.js
@@ -0,0 +1,10 @@
+import styledComponents from 'styled-components';
+
+export const ContainerResult = styledComponents.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+`;
