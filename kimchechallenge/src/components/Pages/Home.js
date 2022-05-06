74e7961d740bb903583b241a58a094360a2451b3
MuriloM <muriloommaia@gmail.com>
2022-05-06 00:00:04 -0300

    feat: implement in Home Container and Search

--- kimchechallenge/src/components/Pages/Home.js
+++ kimchechallenge/src/components/Pages/Home.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { ContainerResult } from '../Containers/Container.Result';
 import Filters from '../Organisms/Filters';
 import Header from '../Organisms/Header';
+import SearchResult from '../Organisms/SearchResult';
 
 export default function Home() {
   return (
     <div>
       <Header />
       <Filters />
+      <ContainerResult>
+        <SearchResult />
+      </ContainerResult>
     </div>
   );
 }
