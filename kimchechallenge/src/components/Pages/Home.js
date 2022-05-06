be64af2fb3377adee7ec3cac48ac0029eaf3c54a
MuriloM <muriloommaia@gmail.com>
2022-05-06 09:43:01 -0300

    feat: create useEffect to getCountries

--- kimchechallenge/src/components/Pages/Home.js
+++ kimchechallenge/src/components/Pages/Home.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useCallback, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getCountries } from '../../redux/slices/country.slice';
+import { setFilter } from '../../redux/slices/filter.slice';
 import { ContainerResult } from '../Containers/Container.Result';
 import Filters from '../Organisms/Filters';
 import Header from '../Organisms/Header';
 import SearchResult from '../Organisms/ResultField/SearchResult';
 
 export default function Home() {
+  const dispatch = useDispatch();
+  const countries = useSelector((state) => state.countries.data);
+  const { continent, search } = useSelector((state) => state.filter);
+  useEffect(() => {
+    dispatch(getCountries());
+  }, [dispatch, countries]);
   return (
     <div>
       <Header />
