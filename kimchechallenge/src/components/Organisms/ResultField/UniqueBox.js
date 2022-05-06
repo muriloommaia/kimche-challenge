4ec64849672bd028ac3a38fb3455a76abf3847c2
MuriloM <muriloommaia@gmail.com>
2022-05-06 11:58:11 -0300

    feat: make the uniquebox content

--- kimchechallenge/src/components/Organisms/ResultField/UniqueBox.js
+++ kimchechallenge/src/components/Organisms/ResultField/UniqueBox.js
@@ -1,18 +1,42 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable import/no-extraneous-dependencies */
 import PropTypes from 'prop-types';
 import React from 'react';
+import { Paragraph } from '../../Atoms/Paragraph';
+import { BoxOneCountry } from '../../Containers/Container.Unique';
 
 export default function UniqueBox({ country }) {
   const {
-    name, emoji,
+    name, emoji, native, states, languages, code, currency,
   } = country;
   return (
-    <div>
-      <p>
+    <BoxOneCountry>
+      <Paragraph>
         {name}
         {emoji}
-      </p>
-    </div>
+      </Paragraph>
+      <Paragraph>
+        {native}
+      </Paragraph>
+      <Paragraph>
+        Code:
+        { ' ' }
+        {code}
+      </Paragraph>
+      <Paragraph>
+        {currency}
+      </Paragraph>
+      <Paragraph>
+        Number of languages:
+        { ' ' }
+        {states.length > 0 ? states.length : 'No states'}
+      </Paragraph>
+      <Paragraph>
+        Number of languages:
+        { ' '}
+        {languages.length > 0 ? languages.length : 'No languages'}
+      </Paragraph>
+    </BoxOneCountry>
   );
 }
 
