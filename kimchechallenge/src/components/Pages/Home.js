6d30b652177154b5b653ee938f6f706a4b17a760
MuriloM <muriloommaia@gmail.com>
2022-05-05 23:09:07 -0300

    feat: implement Filter into Home

--- kimchechallenge/src/components/Pages/Home.js
+++ kimchechallenge/src/components/Pages/Home.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import Filters from '../Organisms/Filters';
+import Header from '../Organisms/Header';
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <Header />
+      <Filters />
+    </div>
   );
 }
