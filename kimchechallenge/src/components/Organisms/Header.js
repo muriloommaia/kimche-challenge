9aa4726b9aa782baf0a33f1319c73b3efc82b525
MuriloM <muriloommaia@gmail.com>
2022-05-05 22:53:52 -0300

    feat: create organism Header

---
+++ kimchechallenge/src/components/Organisms/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Title } from '../Atoms/Title';
+
+export default function Header() {
+  return (
+    <div>
+      <Title>
+        Country search
+      </Title>
+    </div>
+  );
+}
