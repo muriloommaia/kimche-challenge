b51ad9732c31209d10e982c64515a2e65e7d40d6
MuriloM <muriloommaia@gmail.com>
2022-05-07 11:59:26 -0300

    feat: implement ThemeProvider

---
+++ kimchechallenge/src/Style/themeStyle.js
@@ -0,0 +1,12 @@
+import points from '../assets/images/points.png';
+
+export const theme = {
+  colors: {
+    primary: '#2F80AC',
+    secondary: 'black',
+    tertiary: '#000',
+  },
+  textures: {
+    primary: points,
+  },
+};
