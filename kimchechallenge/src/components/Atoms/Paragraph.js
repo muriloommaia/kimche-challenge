7bbcede2b95dcff02b7f9f662a6c7cf90fd3ae20
MuriloM <muriloommaia@gmail.com>
2022-05-07 11:38:52 -0300

    fix: rename styledComponents to styled

--- kimchechallenge/src/components/Atoms/Paragraph.js
+++ kimchechallenge/src/components/Atoms/Paragraph.js
@@ -1,5 +1,5 @@
-import styledComponents from 'styled-components';
+import styled from 'styled-components';
 
-export const Paragraph = styledComponents.p`
+export const Paragraph = styled.p`
   color: ${({ color }) => (color || '#000')};
 `;
