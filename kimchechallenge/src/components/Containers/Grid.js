138f6149c8514bfd3bd7b233b39be28a1bff50e2
MuriloM <muriloommaia@gmail.com>
2022-05-08 20:25:51 -0300

    fix: change media grid

--- kimchechallenge/src/components/Containers/Grid.js
+++ kimchechallenge/src/components/Containers/Grid.js
@@ -2,15 +2,16 @@ import styled from 'styled-components';
 
 export const GridComponent = styled.div`
   display: grid;
+  width: 100%;
   grid-template-columns: repeat(1, minmax(0, 1fr));
   @media (min-width: 684px) {
     grid-template-columns: repeat(2, minmax(0, 1fr));
   }
-    @media (min-width: 920px) {
+  @media (min-width: 920px) {
     grid-template-columns: repeat(3, minmax(0, 1fr));
   }
   @media (min-width: 1260px) {
-    grid-template-columns: repeat(4, minmax(0, 1fr));
+    grid-template-columns: repeat(3, minmax(0, 1fr));
   }
   gap: 1rem;
   padding: ${({ padding }) => padding};
