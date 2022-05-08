a972e9081ed4b6e538522ed7c06b2646bb177c27
MuriloM <muriloommaia@gmail.com>
2022-05-08 17:44:06 -0300

    feat: apply background container

--- kimchechallenge/src/components/Organisms/Header.js
+++ kimchechallenge/src/components/Organisms/Header.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import { Title } from '../Atoms/Title';
-import { ContainerHeader } from '../Containers/Container.Header';
+import { Background } from '../Containers/Container.Background';
+import { Flex } from '../Containers/Flex';
 
 export default function Header() {
   return (
     <div>
-      <ContainerHeader>
-        <Title>
-          Country search
-        </Title>
-      </ContainerHeader>
+      <Background
+        color='primary'
+        image='primary'
+      >
+        <Flex
+          align='center'
+          padding='4rem 0'
+          justify='center'
+        >
+          <Title>
+            Country search
+          </Title>
+        </Flex>
+      </Background>
     </div>
   );
 }
