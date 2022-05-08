5a451a2b57b237efc313cd7cb28a076a95b319a3
MuriloM <muriloommaia@gmail.com>
2022-05-08 20:21:22 -0300

    feat: set initial variables

--- kimchechallenge/src/Hooks/usePagination/usePagination.js
+++ kimchechallenge/src/Hooks/usePagination/usePagination.js
@@ -1,4 +1,9 @@
 import { useCallback, useEffect, useState } from 'react';
 
 export function usePagination({ items }) {
+  const NUMBER_ITEMS = 6;
+  const [startIndex, setStartIndex] = useState(0);
+  const [endIndex, setEndIndex] = useState(NUMBER_ITEMS);
+  const [nextEnabled, setNextEnabled] = useState(false);
+  const [prevEnabled, setPrevEnabled] = useState(false);
 }
