bc2052388d9b5a6ecf07437800660d9472b4a0aa
MuriloM <muriloommaia@gmail.com>
2022-05-08 17:21:40 -0300

    style: change button border

--- kimchechallenge/src/components/Atoms/Button.js
+++ kimchechallenge/src/components/Atoms/Button.js
@@ -5,7 +5,7 @@ export const Button = styled.button`
   height: auto;
   color: ${({ selected, theme }) => (selected ? theme.colors.secondary : theme.colors.tertiary)};
   margin-left: .5rem;
-  border-radius: 1rem;
+  border-radius: .3rem;
   border: 2px solid;
   border-color: black;
   background-color: ${({ selected, theme }) => (selected ? theme.colors.primary : theme.colors.secondary)};
