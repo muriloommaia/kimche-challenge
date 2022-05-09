19f1730c60479fa20f81d81e0ff717a4196b250c
MuriloM <muriloommaia@gmail.com>
2022-05-09 00:31:19 -0300

    fix: ajust media screen

--- kimchechallenge/src/components/Organisms/ResultField/BoxGroup.js
+++ kimchechallenge/src/components/Organisms/ResultField/BoxGroup.js
@@ -5,6 +5,7 @@ import { usePagination } from '../../../Hooks/usePagination/usePagination';
 import { Button } from '../../Atoms/Button';
 import { Line } from '../../Atoms/Line';
 import { Paragraph } from '../../Atoms/Paragraph';
+import { MediaScreen } from '../../Containers/Container.media';
 import { Flex } from '../../Containers/Flex';
 import { GridComponent } from '../../Containers/Grid';
 import UniqueBox from './UniqueBox';
@@ -19,74 +20,79 @@ export default function BoxGroup({ items, name }) {
     endIndex,
   } = usePagination({ items: items.length });
   return (
-    <Flex
-      direction='column'
-      padding='0px'
+    <MediaScreen
+      xlWidth='1280px'
+      xlMargin='auto'
     >
       <Flex
-        align='center'
-        justify='space-between'
-        width='98vw'
+        direction='column'
+        padding='0px'
       >
-        <Line
-          width='3.75rem'
-          borderWidth='3px'
-          height='4px'
-          color='primary'
-        />
-        <Paragraph
-          fontSize='2.0rem'
-          text='center'
-          padding='0 0.5rem'
+        <Flex
+          align='center'
+          justify='space-between'
+          width='98vw'
         >
-          {name}
-        </Paragraph>
-        <Line
-          flex='2 2 auto'
-          borderWidth='3px'
-          height='4px'
-          color='primary'
-        />
+          <Line
+            width='3.75rem'
+            borderWidth='3px'
+            height='4px'
+            color='primary'
+          />
+          <Paragraph
+            fontSize='2.0rem'
+            text='center'
+            padding='0 0.5rem'
+          >
+            {name}
+          </Paragraph>
+          <Line
+            flex='2 2 auto'
+            borderWidth='3px'
+            height='4px'
+            color='primary'
+          />
+        </Flex>
+        <Flex>
+          <Button
+            type='button'
+            onClick={setPrevPage}
+            disabled={!prevEnabled}
+            hoverColor='primary'
+            border='0px'
+            hidden={!prevEnabled}
+            fontSize='2.0rem'
+            color='primary'
+          >
+            <i className='fa-solid fa-angle-left' />
+
+          </Button>
+
+          <GridComponent
+            cols={3}
+            padding='1rem 2rem'
+          >
+            {items.slice(startIndex, endIndex).map((country) => (
+              <UniqueBox key={country.name} country={country} />
+            ))}
+          </GridComponent>
+          <Button
+            type='button'
+            onClick={setNextPage}
+            disabled={!nextEnabled}
+            border='0px'
+            hidden={!nextEnabled}
+            hoverColor='primary'
+            fontSize='2.0rem'
+            color='primary'
+          >
+            <i className='fa-solid fa-angle-right' />
+          </Button>
+
+        </Flex>
+
       </Flex>
-      <Flex>
-        <Button
-          type='button'
-          onClick={setPrevPage}
-          disabled={!prevEnabled}
-          hoverColor='primary'
-          border='0px'
-          hidden={!prevEnabled}
-          fontSize='2.0rem'
-          color='primary'
-        >
-          <i className='fa-solid fa-angle-left' />
-
-        </Button>
-
-        <GridComponent
-          cols={3}
-          padding='1rem 2rem'
-        >
-          {items.slice(startIndex, endIndex).map((country) => (
-            <UniqueBox key={country.name} country={country} />
-          ))}
-        </GridComponent>
-        <Button
-          type='button'
-          onClick={setNextPage}
-          disabled={!nextEnabled}
-          border='0px'
-          hidden={!nextEnabled}
-          hoverColor='primary'
-          fontSize='2.0rem'
-          color='primary'
-        >
-          <i className='fa-solid fa-angle-right' />
-        </Button>
-
-      </Flex>
-
-    </Flex>
+    </MediaScreen>
   );
 }
 
