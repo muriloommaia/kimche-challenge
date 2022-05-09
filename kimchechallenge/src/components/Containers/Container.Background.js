b8ae6f491123b58a2f731d3721618aaf716692cf
MuriloM <muriloommaia@gmail.com>
2022-05-08 22:42:31 -0300

    style: add property into Background Container

--- kimchechallenge/src/components/Containers/Container.Background.js
+++ kimchechallenge/src/components/Containers/Container.Background.js
@@ -3,4 +3,5 @@ import styled from 'styled-components';
 export const Background = styled.div`
   background-image: url(${({ theme, image }) => theme.textures[image]});
   background-color: ${({ theme, color }) => theme.colors[color]};
+  width: ${({ width }) => width};
 `;
